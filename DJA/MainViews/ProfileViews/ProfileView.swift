549ce7c21aad9861d5940c466df0dc9055998642
St Arnaud, BJ, Mr [25116835@sun.ac.za] <25116835@sun.ac.za>
2024-06-10 16:10:43 +0200

    fixed and improved the profile page

--- DJA/MainViews/ProfileViews/ProfileView.swift
+++ DJA/MainViews/ProfileViews/ProfileView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
+import UIKit
 
 struct ProfileView: View {
+    @State private var profileImage: UIImage? = UIImage(named: "defaultProfileImage") // Placeholder image
+    @State private var isImagePickerPresented = false
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
@@ -15,20 +19,90 @@ struct ProfileView: View {
                         .foregroundColor(.blue)
                         .shadow(color: .black.opacity(0.2), radius: 2, x: 1, y: 1)
                 }
-
                 
                 List {
                     Section(header: Text("User Information")) {
-                        HStack {
-                            Text("Name:")
-                            Spacer()
-                            Text("Ben St Arnaud") // Example data
+                        VStack {
+                            Button(action: {
+                                isImagePickerPresented = true
+                            }) {
+                                if let profileImage = profileImage {
+                                    Image(uiImage: profileImage)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 100, height: 100)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                                        .shadow(radius: 10)
+                                        .padding(.top, 10)
+                                } else {
+                                    Image(systemName: "person.circle.fill")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(.gray)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                                        .shadow(radius: 10)
+                                        .padding(.top, 10)
+                                }
+                            }
+                            .sheet(isPresented: $isImagePickerPresented) {
+                                CustomImagePicker(selectedImage: $profileImage)
+                            }
                         }
-                        HStack {
-                            Text("Email:")
-                            Spacer()
-                            Text("benstarnaudwork@gmail.com") // Example data
+                        .frame(maxWidth: .infinity) // Center the profile image
+                        
+                        VStack(alignment: .leading, spacing: 15) {
+                            HStack {
+                                Text("Name:")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Text("Ben St Arnaud") // Example data
+                                    .font(.body)
+                                    .foregroundColor(.primary)
+                            }
+                            .padding(.vertical, 5)
+                            
+                            HStack {
+                                Text("Email:")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Text("benstarnaudwork@gmail.com") // Example data
+                                    .font(.body)
+                                    .foregroundColor(.primary)
+                            }
+                            .padding(.vertical, 5)
+                            
+                            HStack {
+                                Text("Phone:")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Text("+1 123 456 7890") // Example data
+                                    .font(.body)
+                                    .foregroundColor(.primary)
+                            }
+                            .padding(.vertical, 5)
+                            
+                            HStack {
+                                Text("Date of Birth:")
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Text("January 1, 2000") // Example data
+                                    .font(.body)
+                                    .foregroundColor(.primary)
+                            }
+                            .padding(.vertical, 5)
                         }
+                        .padding()
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+
                     }
 
                     NavigationLink(destination: PaymentBankingDetailsView()) {
@@ -62,7 +136,7 @@ struct PaymentBankingDetailsView: View {
                 HStack {
                     Text("Payment Method:")
                     Spacer()
-                    Text("Bens's Credit Card") // Example data
+                    Text("Ben's Credit Card") // Example data
                 }
                 HStack {
                     Text("Billing Address:")
@@ -178,7 +252,46 @@ struct AccountSettingsView: View {
     }
 }
 
-#Preview {
-    ProfileView()
+struct CustomImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+
+    class CustomCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var selectedImage: UIImage?
+        let parent: CustomImagePicker
+
+        init(selectedImage: Binding<UIImage?>, parent: CustomImagePicker) {
+            _selectedImage = selectedImage
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                selectedImage = uiImage
+            }
+            picker.dismiss(animated: true)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
+
+    func makeCoordinator() -> CustomCoordinator {
+        CustomCoordinator(selectedImage: $selectedImage, parent: self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
 }
 
